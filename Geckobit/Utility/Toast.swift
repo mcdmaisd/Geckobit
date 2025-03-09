86b7152e3715f1e752d7376f41f4132945b6684b
mcdmaisd <tkim0156@gmail.com>
2025-03-10 00:54:11 +0900

    feat: toast 기능 추가

---
+++ Geckobit/Utility/Toast.swift
@@ -0,0 +1,32 @@
+//
+//  Toast.swift
+//  Geckobit
+//
+//  Created by ilim on 2025-03-09.
+//
+
+import UIKit
+import Toast
+
+func presentToast(_ message: String) {
+    var style = ToastStyle()
+    
+    style.messageColor = .white
+    style.backgroundColor = .systemPink
+    
+    let window = UIApplication.shared.getCurrentScene()
+    let center = CGPoint(x: window.bounds.midX, y: window.bounds.midY)
+    
+    window.makeToast(message, duration: 1, point: center, title: nil, image: nil, style: style, completion: nil)
+}
+
+func presentLoading() {
+    let window = UIApplication.shared.getCurrentScene()
+    window.makeToastActivity(.center)
+}
+
+func hideLoading() {
+    let window = UIApplication.shared.getCurrentScene()
+    
+    window.hideToastActivity()
+}
