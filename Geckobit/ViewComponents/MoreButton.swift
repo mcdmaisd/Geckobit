236a7da4bff204e7f845335a502798188cf143f6
mcdmaisd <tkim0156@gmail.com>
2025-03-18 09:11:34 +0900

    refactor: 더보기 버튼 글자 크기 및 접근제한자 수정

--- Geckobit/ViewComponents/MoreButton.swift
+++ Geckobit/ViewComponents/MoreButton.swift
@@ -11,7 +11,7 @@ import RxSwift
 
 final class MoreButton: BaseView {
     private let disposeBag = DisposeBag()
-    private(set) var button = UIButton()
+    private let button = UIButton()
     
     override func configureHierarchy() {
         addView(button)
@@ -24,14 +24,24 @@ final class MoreButton: BaseView {
     }
     
     override func configureView() {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: C.medium)
+        
         var config = UIButton.Configuration.plain()
+        
         config.image = UIImage(systemName: C.right)
+        config.preferredSymbolConfigurationForImage = imageConfig
         config.title = C.more
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer {
+            var attributes = $0
+            attributes.font = .systemFont(ofSize: C.medium)
+            return attributes
+        }
+        
         config.imagePlacement = .trailing
         config.baseForegroundColor = .customGray
         config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-        button.configuration = config
         
+        button.configuration = config
         button.rx.tap
             .subscribe { _ in
                 presentToast(C.noData)
